a4afa8aa592d3b7d777ad9f7eead1a2d717d6f08
Adrian Benedicto <adrxyz20@gmail.com>
2024-06-21 23:23:46 +0800

    python simple ordering

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt6 import uic
+from PyQt6.QtWidgets import *
+from backend.Register import benedicto_MainWindow
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = benedicto_MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
